09e6cec78d5cdbcc125f86e14c9fdb220743aeac
Adminha-Dinha <61115300+Adminha-Dinha@users.noreply.github.com>
2020-12-28 07:58:41 -0300

    Adicionar arquivos de projeto.

---
+++ Alura.Loja.Testes.ConsoleApp/LojaContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    public class LojaContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        //essa implementação permite que possa ser usado qualquer tipo de banco
+        public LojaContext()
+        {
+
+        }
+
+
+        //essa implementação permite que possa ser usado qualquer tipo de banco
+
+        public LojaContext(DbContextOptions<LojaContext> options): base(options)
+        {
+
+        }
+
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            //essa implementação permite que possa ser usado qualquer tipo de banco
+            if (!optionsBuilder.IsConfigured)
+            {
+                //se for usar só um banco não precisa da validação OsConfigured
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=LojaDB;Trusted_Connection=true;");  optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=LojaDB;Trusted_Connection=true;"); optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=LojaDB;Trusted_Connection=true;");
+            }
+
+          
+        }
+    }
+}
---
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          using (var contexto = new LojaContext())
+            {
+                var produtos = contexto.Produtos.ToList();
+                foreach(var p in produtos)
+                {
+                    Console.WriteLine(p);
+                }
+
+                Console.WriteLine("----------------");
+
+                foreach (var e in contexto.ChangeTracker.Entries())
+                {
+                    Console.WriteLine(e.State);
+                }
+
+                var p1 = produtos.Last();
+                p1.Nome = "Eu vi o que vcs fizeram";
+
+                //contexto.SaveChanges();
+                Console.WriteLine("----------------");
+
+                foreach (var e in contexto.ChangeTracker.Entries())
+                {
+                    Console.WriteLine(e.State);
+                }
+            }
+        }
+
+       
+    }
+}
